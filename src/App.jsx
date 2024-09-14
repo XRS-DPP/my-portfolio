5478fa27db796d8ef4aa85dcb4e8f4a0d0bcca39
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-09-14 21:54:41 +0100

    installed react router dom, adds Routes, Route components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
 import Header from "./components/Header";
 function App() {
@@ -5,6 +6,13 @@ function App() {
     <>
       <div className={styles.App}>
         <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/projects" element={<Project />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          {/* <Route path="*" element={<Error />} /> */}
+        </Routes>
       </div>
     </>
   );
---
+++ src/components/Menu.jsx
@@ -0,0 +1,12 @@
+const Menu = () => {
+  return (
+    <>
+      <div>
+        <Link to="/Home">Home</Link>
+        <Link></Link>
+      </div>
+    </>
+  );
+};
+
+export default Menu;
